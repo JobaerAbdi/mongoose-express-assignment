ff1372626b0151767f8f615ca0f8a687cee9d591
JobaerAbdi <jobaerrahman757@gmail.com>
2024-05-21 17:15:26 +0600

    Product router controller services create completed

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { ProductRoutes } from "./modules/product/product.route";
 
 const app: Application = express()
 
@@ -7,7 +8,8 @@ const app: Application = express()
 app.use(express.json());
 app.use(cors());
 
-// app.use('/api/v1', )
+app.use('/api', ProductRoutes);
+
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
